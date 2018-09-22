18ee53f319e9d56e774108de139c9e831b9761fc
Rachel Grier <rgrier104@gmail.com>
2018-09-22 23:27:40 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -14,6 +14,7 @@ class Song
 
   def self.new_by_filename(filename)
     filename_array = filename.chomp('.mp3').split(' - ')
+    binding.pry
     song_name = filename_array[1]
     song = self.new(song_name)
     artist_name = filename_array[0]
