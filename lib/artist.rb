b0fa22e4929f153897cd8fa926babd5a70159b50
Rachel Grier <rgrier104@gmail.com>
2018-09-22 23:13:29 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -27,7 +27,7 @@ class Artist
   end
 
   def self.find_or_create_by_name(name)
-    if @@all.detect{|artist| artist.name == name}
+    if all.detect{|artist| artist.name == name}
     
     else
       self.new(name).save
