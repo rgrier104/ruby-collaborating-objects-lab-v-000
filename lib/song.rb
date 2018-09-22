a6d58a04d4f59dfac21b8b4c31c63964e18b13f0
Rachel Grier <rgrier104@gmail.com>
2018-09-22 23:42:12 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -7,9 +7,8 @@ class Song
   end
 
   def artist=(artist)
-    @artist = Artist.find_or_create_by_name(artist)
-    @artist.add_song(self)
-    @artist
+    self.artist = Artist.find_or_create_by_name(artist)
+    self.artist.add_song(self)
   end
 
   def self.new_by_filename(filename)
